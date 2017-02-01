310049fc86bc41f8ae6fb5b18d8284c3f7d7b465
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-02-01 10:14:40 +0100

    Fixed bug in run script

--- tests/mpifitness/run_mpifitness.sh
+++ tests/mpifitness/run_mpifitness.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$MPIFITNESS_CONF" == "x$MPIFITNESS_CONF" ]
+if [ "$MPIFITNESS_CONF" != "x$MPIFITNESS_CONF" ]
 then
-    MPIFITNESS_CONF=/usr/share/tests/mpifitness/small-msgs.conf
+    MPIFITNESS_CONF="/usr/share/tests/mpifitness/small-msgs.conf"
 fi
 
 mpirun "$@" /usr/bin/mpifitness.exe $MPIFITNESS_CONF
