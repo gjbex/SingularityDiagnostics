9484af9ba047977b7900ea97f8816cc9d989bb74
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-02-01 09:56:38 +0100

    Fixed run script

--- tests/mpifitness/run_mpifitness.sh
+++ tests/mpifitness/run_mpifitness.sh
@@ -2,7 +2,7 @@
 
 if [ "$MPIFITNESS_CONF" == "x$MPIFITNESS_CONF" ]
 then
-    MPIFITNESS_CONF=/usr/share/tests/small-msgs.conf
+    MPIFITNESS_CONF=/usr/share/tests/mpifitness/small-msgs.conf
 fi
 
 mpirun "$@" /usr/bin/mpifitness.exe $MPIFITNESS_CONF
